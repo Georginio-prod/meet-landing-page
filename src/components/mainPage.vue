f7c76c0ed75b18129a838b9673c0b3f87640ed87
Georginio-prod <etonameklou6@gmail.com>
2024-08-05 18:55:00 +0000

    style: add the composant for the header page but I don't use the style and color

---
+++ src/components/mainPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-col items-center">
+        <div class="w-0.5 h-10 bg-gray-400">
+        </div>
+        
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name:'Main',
+};
+</script>
