c6b162a9401546884d8856bc6f5f892710b85973
Georginio-prod <etonameklou6@gmail.com>
2024-08-06 00:07:23 +0000

    style: add the color on the style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        title:"#28283D",
+        h2:"#87879D",
+        btn1:"#4D96A9",
+        btn2:"#855FB1",
+        sp:"#8FE3F9",
+        tr:"#D8D8D8",
+        num:"#87879D",
+        gree:"#4D96A9",
+        sur:"#4D96A9",
+        num2:"#D9B8FF"
+      }
+    },
   },
   plugins: [],
 }
